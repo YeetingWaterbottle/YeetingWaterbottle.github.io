5a5a0ea1f2c5a58ca6dd39b206f37922ace1366b
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2022-08-07 00:16:25 -0700

    updated to use ytmusicapi library to get accurate results

--- SpotifyPlaylist-yt-dlp/download.py
+++ SpotifyPlaylist-yt-dlp/download.py
@@ -1,43 +1,47 @@
+try:
+    from ytmusicapi import YTMusic
+except ImportError:
+    import os, sys
+    os.system(f"{sys.executable} -m pip install ytmusicapi")
+    import ytmusicapi
+
 import time
 import concurrent.futures # import multi threading library
 import os
+import json
+from datetime import date
+
+ytmusic = YTMusic()
+
+def get_song_url(query):
+    print(f"Searching Query: {query}")
+    search_result = ytmusic.search(query, "songs", None, 1)
+    song_id = search_result[0]["videoId"]
+    print(f"Found Song ID: {song_id} For Song {query}")
+    song_urls.append(f"https://music.youtube.com/watch?v={song_id}")
 
 song_urls = []
 
-song_list = input("Enter Generated 'songs.txt' Location: ")
+unformatted = json.loads(input("Paste in the list of songs and artists here: "))
+song_list = []
 
-try:
-    with open(song_list) as f:
-        songs = f.read().splitlines()
-        for song in songs:
-            if song.startswith("#"):
-                continue
-            
-            song_urls.append(song)
+for song in unformatted:
+    song_list.append(" ".join(song))
 
-except FileNotFoundError:
-    print("Error: File Not Found.")
-    input("Press Enter to Close.")
-    quit()
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor: # Searches Song on Youtube Music
+    executor.map(get_song_url, song_list)
+
+with open(f"{date.today()} - song_url_list.txt", "wt") as song_url_list: # Writes Result to File
+    song_url_list.write("\n".join(song_urls))
 
 # Customize The Download Command, ex. --audio-format, --audio-quality, and the metadata settings.
-base_download_command = """yt-dlp -fbestaudio --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail --add-metadata -o "%(title)s - %(artist)s.%(ext)s" --playlist-items 1 --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\\\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\\\"\""""
-# For Detailed Explinations, go to https://github.com/yt-dlp/yt-dlp
-# -fbestaudio           download song with the best audio
-# --extract-audio       extract the audio from the video (duh)
-# --audio-format        encode audio into different file formats (mp3, flac, wav, etc.)
-# --audio-quality       the number after this parameter will determine the audio quality. 0 being the best, 10 being the worst.
-# --embed-thumbnail     embed thumbnail to audio file (this does not work with file types like wav)
-# --add-metadata        adds metadata like song name, artist name, album name, etc.
-# -o                    output template
-# --playlist-items      this will limit how many songs will download, in this script it will only download the top result
-# --ppa "Emb...         this will crop the album cover into a square one
+base_download_command = """yt-dlp -fbestaudio --extract-audio --audio-format mp3 --audio-quality 9 --embed-thumbnail --add-metadata -o "%(title)s - %(artist)s.%(ext)s" --playlist-items 1 --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\\\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\\\"\""""
 
 
 def download_song(url):
+    time.sleep(2)
     # executing yt-dlp commands
     os.system(f"{base_download_command} \"{url}\"")
-    time.sleep(2)
 
 # max_worker will determine how many songs will be downloaded at the same time
 with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
